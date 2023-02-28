d9ad7bd24307ce242ccae6107387821c952f9b5f
Shubham Yadav <112643891+ZealousGuy@users.noreply.github.com>
2023-02-28 20:20:10 +0530

    Add files via upload

---
+++ ProgrammingTask1-Basic/Four_division.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+print(a//b) #Integer Division
+print(a/b)  #Float Division
---
+++ ProgrammingTask1-Basic/Three_arithmaticOps.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b) #sum of the two
+    print(a-b) #diff of the two
+    print(a*b) #product of the two
